fc7f6fd571a37e6a1588a98e5f5d14e39683779c
Chelsea Roston <cmroston@gmail.com>
2021-06-22 13:41:03 -0400

    adds create methods and routes for stories and chapters

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,4 +6,12 @@ class StoriesController < ApplicationController
   def show 
     render json: Story.find(params[:id])
   end
+
+  def create 
+    @story = Story.new(
+      title: params[:title],
+      description: params[:description],
+      user_id: current_user.id
+    )
+  end
 end
